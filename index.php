abbc81e031975388df00be2f0ed147ed34ff8271
Kavin2000 <49057422+Kavin2000@users.noreply.github.com>
2019-05-27 13:24:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Car Park Management System</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<?php
+			include('inc/head.php');
+	?>
+</head>
+<body>
+	<section id="container">
+	<?php
+			include('inc/header.php');
+						
+	?>
+	
+	<section id="content">
+	<img src="src/back.png" width="1000" height="500" style="position:absolute; z-index:-1; margin:0;"/>
+	<div>
+		<vhead>Welcome To Car Park Management System </br></vhead>
+	</div>
+	<div style="text-align:center; font-weight:bold; margin-top:310px;font-size:28px;">
+		<p>We offer cashless car parking services in town.</br>Book Now or Unbook</p>
+	</div>
+	</section>
+	
+	</section>
+	
+</body>
+</html>
